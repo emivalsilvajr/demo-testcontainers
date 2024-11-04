7e3db87c3a9798319753a901ffa4f9246f7a7ae3
Emival Silva <emival@gmail.com>
2024-11-03 22:25:30 -0300

    initial commit

---
+++ src/test/java/com/example/demo/testcontainer/DemoTestcontainerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo.testcontainer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoTestcontainerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
